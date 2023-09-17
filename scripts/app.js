a554d3a6e986e3d769790c6d5e76de98381289e2
Valentin SNITH <valentin.snith@etu.unicaen.fr>
2023-09-17 21:50:32 +0200

    Ajout du passage entre le travail et la pause

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
-let minute = 1;
+let minutePause = 5;
+let secondePause = 0;
+let minute = 0;
 let seconde = 5;
+let enPause = false;
 
 const minuteur = document.getElementById("temps");
 const boutonLancer = document.getElementById("lancerBouton");
@@ -19,6 +22,11 @@ affichage(minute, seconde);
 function decompte() {
     affichage(minute, seconde);
     if (seconde == 0) {
+        if (minute == 0) {
+            minute = enPause ? minute : minutePause;
+            seconde = enPause ? seconde : secondeTravail;
+            enPause = !enPause;
+        }
         minute--;
         seconde = 59;
     } else {
